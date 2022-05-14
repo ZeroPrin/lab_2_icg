90120fe7e9c32246cd99cbb0172beccaf43c9f4a
Глеб Панченко <panchenko.glebka@gmail.com>
2022-05-14 10:41:11 +0500

    готово

--- Project1/main.c
+++ Project1/main.c
@@ -12,7 +12,8 @@ GLuint projMatrLoc;
 GLuint rotMatrLoc;
 
 
-void setUniformLocations() {
+void setUniformLocations() // заносим юниформ переменные
+{
     projMatrLoc = glGetUniformLocation(prog, "projMatr");
     rotMatrLoc = glGetUniformLocation(prog, "rotMatr");
 }
@@ -34,7 +35,8 @@ void init()
     int len = sizeof(shaders) / sizeof(shaders[0]); // определяем колво шейдеров
     createProg(shaders, len); // создаём программу, содержащую эти шейдеры
     int i = 0;
-    for (; i < len; i++) {
+    for (; i < len; i++) // удаление шейдеров
+    {
         glDeleteShader(shaders[i]);
     }
 
@@ -51,6 +53,7 @@ void init()
 
 void createBuffer() // функция создания вершин
 {
+    // массив индексов
     GLushort indices[] = 
     {
         0, 1, 2,
@@ -82,14 +85,25 @@ void createBuffer() // функция создания вершин
          1.0f, -1.0f, -1.0f,// right-bottom
 
          // colors
+         
+         //1.0f, 0.0f, 0.0f, 1.0f, // red
+         //0.0f, 1.0f, 0.0f, 1.0f, // green
+         //1.0f, 0.0f, 1.0f, 1.0f, // purple
+         //1.0f, 1.0f, 0.0f, 1.0f, // yellow
+         //0.0f, 1.0f, 1.0f, 1.0f, // aqua
+         //0.5f, 0.0f, 0.0f, 1.0f, // dark-red
+         //0.0f, 0.5f, 0.0f, 1.0f, // dark-green
+         //0.0f, 0.0f, 0.5f, 1.0f  // dark-blue
+
+         
+         1.0f, 0.0f, 0.0f, 1.0f, // red
          1.0f, 0.0f, 0.0f, 1.0f, // red
          0.0f, 1.0f, 0.0f, 1.0f, // green
-         1.0f, 0.0f, 1.0f, 1.0f, // purple
-         1.0f, 1.0f, 0.0f, 1.0f, // yellow
-         0.0f, 1.0f, 1.0f, 1.0f, // aqua
-         0.5f, 0.0f, 0.0f, 1.0f, // dark-red
-         0.0f, 0.5f, 0.0f, 1.0f, // dark-green
-         0.0f, 0.0f, 0.5f, 1.0f  // dark-blue
+         0.0f, 1.0f, 0.0f, 1.0f, // green
+         0.0f, 0.0f, 1.0f, 0.0f, // blue
+         0.0f, 0.0f, 1.0f, 0.0f, // blue
+         1.0f, 1.0f, 0.0f, 1.0f, // dark-green
+         1.0f, 1.0f, 0.0f, 1.0f  // dark-blue
     };
 
     glGenBuffers(1, &vbo); // просим создать 1 объект и идентификатор положить в переменную vbo
@@ -98,21 +112,21 @@ void createBuffer() // функция создания вершин
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo); // массив индексов ibo
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); // такая же функция заполнения
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 void initVao() {
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
+    glGenVertexArrays(1, &vao); // генерируем объект вао
+    glBindVertexArray(vao); // бинди, чтобы настройки применялись
 
     // vertex array object will store next things:
     // 1. Binding to GL_ELEMENT_ARRAY_BUFFER
     // 2. Enabled vertex attrib arrays
     // 3. Vertex attrib pointer settings
 
-    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(0); // сохраняем эти настройки в вао
     glEnableVertexAttribArray(1);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     // glVertexAttribPointer will use GL_ARRAY_BUFFER
@@ -122,27 +136,49 @@ void initVao() {
     // Binding to GL_ARRAY_BUFFER will not be stored in
     // the vertex array object,
     // but it was already defined for glVertexAttribPointer.
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0); // убираем буфер
 
     // This will be used by glDrawElements
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo); // биндим ещё один буфер
 
-    glBindVertexArray(0);
+    glBindVertexArray(0); // убираем буфер
 
 }
 
 float* rotationMatr(float elapsed) 
 {
-    float part = fmodf(elapsed, 5.0f) / 5.0f;
+    float part = fmodf(elapsed, 5.0f) / 5.0f; //обновление каждые 5 секунд
     float angle = 3.14159f * 2.0f * part;
-    float res[] = {
+    
+    // вращение по у
+    /*float res[] = 
+    {
         cosf(angle), 0, -sinf(angle), 0,
         0, 1, 0, 0,
         sinf(angle), 0, cosf(angle), 0,
         0, 0, 0, 1
+    };*/
+
+    // вращение по х
+    /*float res[] =
+    {
+        1, 0, 0, 0,
+        0, cosf(angle), -sinf(angle), 0,
+        0, sinf(angle), cosf(angle), 0,
+        0, 0, 0, 1
+    };*/
+
+    // вращение по z
+    float res[] =
+    {
+        cosf(angle), -sinf(angle), 0, 0,
+        sinf(angle), cosf(angle), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
     };
-    float* matr = malloc(sizeof(float) * 16);
-    memcpy(matr, res, sizeof(float) * 16);
+
+    float* matr = malloc(sizeof(float) * 16); // выделяем место
+    memcpy(matr, res, sizeof(float) * 16);// копируем
     return matr;
 }
 
@@ -154,11 +190,11 @@ void display() {
     glBindBuffer(GL_ARRAY_BUFFER, 0); //задействуем массив вершин
 
     glBindVertexArray(vao);
-    float elapsed = glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
-    float* matr = rotationMatr(elapsed);
+    float elapsed = glutGet(GLUT_ELAPSED_TIME) / 1000.0f; // берём время
+    float* matr = rotationMatr(elapsed); // отправляем
     glUniformMatrix4fv(rotMatrLoc, 1, GL_FALSE, matr);
-    free(matr);
-    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, 0);
+    free(matr); // стираем
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, 0); // рисовать по индексам
 
     glBindVertexArray(0); // деактивируем
     glUseProgram(0); // деактивируем
@@ -166,39 +202,34 @@ void display() {
     glutPostRedisplay();
 }
 
-float toRad(float deg) {
-    // 180 deg = pi
-    // "deg" deg = x
+float toRad(float deg) // конвертация в радианы
+{
     return deg * 3.141593f / 180.0f;
 }
 
-void reshape(int w, int h) {
-    float n = 0.2f;
-    float f = 1000.0f;
-    float fov = toRad(90);
+void reshape(int w, int h) // функция изменения размера окна
+{
+    float n = 0.2f; // расположение ближней плоскости
+    float f = 1000.0f;// расположение дальней плоскости
+    float fov = toRad(90); // поле зрение
     float aspect = w / (float)h;
     float t = n * tanf(fov / 2.0f);
     float r = t * aspect;
 
-    float projMatr[] = {
+    // проекционная матрица (пишем по столбцам)
+    float projMatr[] = 
+    {
         n / r, 0,   0,              0,
         0,   n / t, 0,              0,
         0,   0,   (f + n) / (n - f),    -1,
         0,   0,   2.0f * n * f / (n - f), 0
     };
-    printf("n=%.1f  f=%.1f  fov=%.2f  aspect=%.2f  t=%.2f  r=%.2f\n", n, f, fov, aspect, t, r);
-    glUseProgram(prog);
-    glUniformMatrix4fv(projMatrLoc, 1, GL_FALSE, projMatr);
-    glUseProgram(0);
-    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-}
 
-void keyboard(unsigned char key, int x, int y) {
-    (void)x;
-    (void)y;
-    if (key == 27) {
-        glutLeaveMainLoop();
-    }
+    //printf("n=%.1f  f=%.1f  fov=%.2f  aspect=%.2f  t=%.2f  r=%.2f\n", n, f, fov, aspect, t, r);
+    glUseProgram(prog); // указываем программу
+    glUniformMatrix4fv(projMatrLoc, 1, GL_FALSE, projMatr); // передаём значения в projMatrLoc
+    glUseProgram(0); // деактивируем
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);// указания, как нужно выполнять трансформацию в оконные кординаты
 }
 
 int main(int argc, char* argv[]) 
@@ -209,7 +240,7 @@ int main(int argc, char* argv[])
     glutInitWindowPosition(100, 100);// позиция 100 100
     glutCreateWindow("кубик");// создание окна с именем 
 
-    GLenum res = glewInit();
+    GLenum res = glewInit(); // проверка на ошибки
     if (res != GLEW_OK) {
         fprintf(stderr, "Error: %s\n", glewGetErrorString(res));
         return 1;
@@ -218,7 +249,6 @@ int main(int argc, char* argv[])
 
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
-    glutKeyboardFunc(keyboard);
     init();
     glutMainLoop(); //запустить цикл обработки сообщений
     return 0;
